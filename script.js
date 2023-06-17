eafc761220ecb273afbda741c3bbae3650404ae0
bhanuchandarsalkam <133007383+bhanuchandarsalkam@users.noreply.github.com>
2023-06-17 12:22:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function palindrome(str){
     var cleaned = str.replace(/[^A-Za-z0-9]/g, '').toLowerCase();
 
     // reverse the cleaned string
-    var reversed = cleaned.split('').reverse().join('');
+    var reversed = cleaned.split('').reverse().join();
 
     // compare the cleaned string with the reversed string
     return cleaned === reversed;
